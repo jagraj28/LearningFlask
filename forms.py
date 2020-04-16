fd50f41ae87dad4394a6b8f3fefa3533db63bb4c
Jagraj Singh <singhjagraj900@gmail.com>
2020-04-17 00:44:47 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    password = StringField("Password", validators = [DataRequired()])
+    confirm_password
