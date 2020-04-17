24ed084b740c7884e091eda4d5d5c6a675324dcd
Jagraj Singh <singhjagraj900@gmail.com>
2020-04-17 21:23:14 +0100

    Update LearningFlask.py

--- LearningFlask.py
+++ LearningFlask.py
@@ -1,21 +1,45 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "2a7a99b002192c5cdde859a03a018c0f"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', {self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
 
 posts = [   
     {
-        'Author': 'Jagraj Singh',
-        'Title': 'Blog Post 1',
-        'Content': 'First Post Content',
-        'Date': '16/04/2020'
+        'author': 'Jagraj Singh',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '16/04/2020'
     },
     {
-        'Author': 'Kalraj Singh',
-        'Title': 'Blog Post 2',
-        'Content': 'Second Post Content',
-        'Date': '18/04/2020',
+        'author': 'Kalraj Singh',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '18/04/2020',
     }   
 ]
 
